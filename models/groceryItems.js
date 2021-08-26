1cff10f4df90f5c59aa828aca6d6bbc376877272
adaruiz <ada.ruiz93@gmail.com>
2021-08-26 15:19:29 -0700

    created schema

---
+++ models/groceryItems.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+
+const groceryItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    default: 1
+  },
+  isPurchased: {
+    type: Boolean
+  },
+  isOrganic: {
+    type: Boolean
+}, {timestamps: true}
+})
+
+
+const GroceryItem = mongoose.model('GroceryItem', groceryItemSchema)
+
+module.exports = GroceryItem

7fcc259c595cf4e0f2f92e7ce2606d135395d54e
metha <matt.ksmp@gmail.com>
2021-08-30 16:39:38 -0700

    uddate some code on home.ejs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const methodOverride = require('method-override')
 const db = require('./db')
 const {groceryItemsController, shoppingListController} = require('./controllers/index')
+const groceryItem = require('./models/groceryItems')
+const shoppingList = require('./models/shoppingList')
 
 app.use(express.static(__dirname + '/public'))
 app.use(methodOverride('_method'))
@@ -11,8 +13,27 @@ app.use(express.static('public'))
 app.use(express.json())
 app.use('/grocery-items', groceryItemsController)
 app.use('/shopping-list', shoppingListController)
+
+// testing code //
+const categoryItems = ['Fruits', 'Vegetables', 'Meat', 'Fish', 'Frozen Foods', 'Bread & Bakery', 'Beverages', 'Baking', 'Personal Care', 'Household Supplies', 'Baby Items', 'Pasta/Rice']
+
+// Create Home Page Route
 app.get('/', (req,res)=>{
-    res.render('home.ejs')
+    groceryItem.find({}, (error, allGroceryItems) => {
+        if (error) {
+            res.send(error);
+        } else {
+            shoppingList.find({}, (error, shoppingList) => {
+                if (error) {
+                    res.send(error);
+                } else {
+                    res.render('home.ejs', { category: categoryItems, groceryItem: allGroceryItems, shoppingList: shoppingList })
+                }
+            })
+        }
+
+    })
+    
 })
 
 app.listen(3088, ()=>console.log('connected to express app on port 3088'))
